aeccf41fb01c8911cd143909b1c36e4b8d6c53ac
Josef Svenningsson <josef.svenningsson@gmail.com>
2017-10-17 13:35:20 +0200

    Clean up monads and language extensions
    Move monads to their own module and remove ununsed monad imports. LogicT
    is completely gone now.
    
    I've also removed unneeded language extensions from the interpreter.

---
+++ AbstractionMonads.hs
@@ -0,0 +1,138 @@
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE FunctionalDependencies     #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module AbstractionMonads where
+
+import Control.Monad.State
+import Control.Applicative
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Except
+import Control.Monad.Writer
+import Data.Functor.Identity
+
+----------------------------------------
+-- Store Monad
+----------------------------------------
+
+newtype StateTStore s m a = StateTStore (StateT s m a)
+  deriving (Functor, Applicative, Monad, MonadTrans, MonadFix,
+            MonadPlus, Alternative, MonadIO,
+            MonadReader r, MonadError e, MonadWriter w)
+
+evalStateTStore (StateTStore m) s = evalStateT m s
+evalStateStore  (StateTStore m) s  = evalState  m s
+
+runStateTStore (StateTStore m) s = runStateT m s
+runStateStore  (StateTStore m) s = runState  m s
+
+class Monad m => MonadStateStore s m | m -> s where
+  getStore :: m s
+  putStore :: s -> m ()
+  updateStore :: (s -> s) -> m ()
+
+instance Monad m => MonadStateStore s (StateTStore s m) where
+  getStore = StateTStore get
+  putStore s = StateTStore (put s)
+  updateStore f = StateTStore (modify f)
+
+instance MonadStateStore s m => MonadStateStore s (ReaderT e m) where
+  getStore = ReaderT (\_ -> getStore)
+  putStore s = ReaderT (\_ -> putStore s)
+  updateStore f = ReaderT (\_ -> updateStore f)
+
+instance MonadStateStore s m => MonadStateStore s (ExceptT e m) where
+  getStore = lift getStore
+  putStore = lift . putStore
+  updateStore = lift . updateStore
+
+----------------------------------------
+-- StateTDead
+----------------------------------------
+
+newtype StateTDead s m a = StateTDead (StateT s m a)
+  deriving (Functor, Applicative, Monad, MonadTrans, MonadFix,
+            MonadPlus, Alternative, MonadIO,
+            MonadReader r, MonadError e, MonadWriter w)
+
+runStateTDead :: StateTDead s m a -> s -> m (a, s)
+runStateTDead (StateTDead m) s = runStateT m s
+
+mkStateTDead m = StateTDead (StateT m)
+
+class Monad m => MonadStateDead s m | m -> s where
+  getDead :: m s
+  putDead :: s -> m ()
+  updateDead :: (s -> s) -> m ()
+
+instance Monad m => MonadStateDead s (StateTDead s m) where
+  getDead = StateTDead get
+  putDead s = StateTDead (put s)
+  updateDead f = StateTDead (modify f)
+
+instance MonadStateDead s m => MonadStateDead s (ReaderT e m) where
+  getDead = ReaderT (\_ -> getDead)
+  putDead s = ReaderT (\_ -> putDead s)
+  updateDead f = ReaderT (\_ -> updateDead f)
+
+instance MonadStateDead s m => MonadStateDead s (ExceptT e m) where
+  getDead = lift getDead
+  putDead = lift . putDead
+  updateDead = lift . updateDead
+
+instance MonadStateDead s m => MonadStateDead s (StateTStore s' m) where
+  getDead = lift getDead
+  putDead = lift . putDead
+  updateDead = lift . updateDead
+
+----------------------------------------
+-- Nondeterminism monad
+----------------------------------------
+
+newtype ND m a =
+  ND { unND :: forall r. (a -> m r -> m r) -> m r -> m r }
+
+runND :: ND Identity a -> [a]
+runND (ND f) = runIdentity $
+  f (\a (Identity as) -> Identity (a:as)) (Identity [])
+
+instance Monad (ND m) where
+  return a = ND (\k -> k a)
+  ND f >>= g = ND (\k -> f (\a -> unND (g a) k))
+
+instance Applicative (ND m) where
+  pure = return
+  (<*>) = ap
+
+instance Functor (ND m) where
+  fmap f (ND g) = ND (\k -> g (\a -> k (f a)))
+
+instance MonadPlus (ND m) where
+  mzero = ND (\sk fk -> fk)
+  ND f `mplus` ND g = ND (\sk fk -> f (\a fk' -> sk a fk')
+                                      (g (\a fk' -> sk a fk') fk))
+
+instance Alternative (ND m) where
+  empty = mzero
+  (<|>) = mplus
+
+----------------------------------------
+-- Specialized reader monad
+----------------------------------------
+-- Currently not used.
+
+newtype ReaderTEnv r m a = ReaderTEnv (ReaderT r m a)
+  deriving (Functor, Applicative, Monad, MonadTrans, MonadFix,
+            MonadPlus, Alternative, MonadIO,
+            MonadState s, MonadError e, MonadWriter w)
+
+class Monad m => MonadReaderEnv e m | m -> e where
+  askEnv   :: m e
+  localEnv :: (e -> e) -> m a -> m a
+
+instance Monad m => MonadReaderEnv e (ReaderTEnv e m) where
+  askEnv = ReaderTEnv ask
+  localEnv f (ReaderTEnv r) = ReaderTEnv (local f r)
--- Interp.hs
+++ Interp.hs
@@ -1,21 +1,13 @@
 {-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE NoMonomorphismRestriction  #-}
 {-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE FunctionalDependencies     #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE RankNTypes                 #-}
-{-# LANGUAGE UndecidableInstances       #-}
 module Interp2 where
 
 import Control.Monad.Reader
 import Control.Monad.State
 import Control.Monad.Except
 import Control.Monad.Writer
-import Control.Monad.Logic
---import MyLogicT
+import AbstractionMonads
 import Data.IntMap as Map
 import Data.Set as Set
 import Control.Applicative
@@ -259,139 +251,3 @@ forP t body = Prelude.foldr (\a r -> body a `mplus` r) mzero t
 abstRun m = runND (runStateTStore (runExceptT (runReaderT m [])) Map.empty)
 
 evalAbst e = abstRun (fix (ev deltaAbst storeNd allocAbst) e)
-
--- Specialized reader monads
-
-newtype ReaderTEnv r m a = ReaderTEnv (ReaderT r m a)
-  deriving (Functor, Applicative, Monad, MonadTrans, MonadFix,
-            MonadPlus, Alternative, MonadIO,
-            MonadState s, MonadError e, MonadWriter w)
-
-class Monad m => MonadReaderEnv e m | m -> e where
-  askEnv   :: m e
-  localEnv :: (e -> e) -> m a -> m a
-
-instance Monad m => MonadReaderEnv e (ReaderTEnv e m) where
-  askEnv = ReaderTEnv ask
-  localEnv f (ReaderTEnv r) = ReaderTEnv (local f r)
-
--- Specialized state monads
-
-newtype StateTStore s m a = StateTStore (StateT s m a)
-  deriving (Functor, Applicative, Monad, MonadTrans, MonadFix,
-            MonadPlus, Alternative, MonadIO,
-            MonadReader r, MonadError e, MonadWriter w)
-
-evalStateTStore (StateTStore m) s = evalStateT m s
-evalStateStore  (StateTStore m) s  = evalState  m s
-
-runStateTStore (StateTStore m) s = runStateT m s
-runStateStore  (StateTStore m) s = runState  m s
-
-class Monad m => MonadStateStore s m | m -> s where
-  getStore :: m s
-  putStore :: s -> m ()
-  updateStore :: (s -> s) -> m ()
-
-instance Monad m => MonadStateStore s (StateTStore s m) where
-  getStore = StateTStore get
-  putStore s = StateTStore (put s)
-  updateStore f = StateTStore (modify f)
-
-instance MonadStateStore s m => MonadStateStore s (ReaderT e m) where
-  getStore = ReaderT (\_ -> getStore)
-  putStore s = ReaderT (\_ -> putStore s)
-  updateStore f = ReaderT (\_ -> updateStore f)
-
-instance MonadStateStore s m => MonadStateStore s (ExceptT e m) where
-  getStore = lift getStore
-  putStore = lift . putStore
-  updateStore = lift . updateStore
-
--- StateTDead
-
-newtype StateTDead s m a = StateTDead (StateT s m a)
-  deriving (Functor, Applicative, Monad, MonadTrans, MonadFix,
-            MonadPlus, Alternative, MonadIO,
-            MonadReader r, MonadError e, MonadWriter w)
-
-runStateTDead :: StateTDead s m a -> s -> m (a, s)
-runStateTDead (StateTDead m) s = runStateT m s
-
-mkStateTDead m = StateTDead (StateT m)
-
-class Monad m => MonadStateDead s m | m -> s where
-  getDead :: m s
-  putDead :: s -> m ()
-  updateDead :: (s -> s) -> m ()
-
-instance Monad m => MonadStateDead s (StateTDead s m) where
-  getDead = StateTDead get
-  putDead s = StateTDead (put s)
-  updateDead f = StateTDead (modify f)
-
-instance MonadStateDead s m => MonadStateDead s (ReaderT e m) where
-  getDead = ReaderT (\_ -> getDead)
-  putDead s = ReaderT (\_ -> putDead s)
-  updateDead f = ReaderT (\_ -> updateDead f)
-
-instance MonadStateDead s m => MonadStateDead s (ExceptT e m) where
-  getDead = lift getDead
-  putDead = lift . putDead
-  updateDead = lift . updateDead
-
-instance MonadStateDead s m => MonadStateDead s (StateTStore s' m) where
-  getDead = lift getDead
-  putDead = lift . putDead
-  updateDead = lift . updateDead
-
-{-
-instance MonadLogic m => MonadLogic (StateTDead s m) where
-  msplit  sm = mkStateTDead $ \s ->
-    do r <- msplit (runStateTDead sm s)
-       case r of
-         Nothing          -> return (Nothing, s)
-         Just ((a,s'), m) ->
-             return (Just (a, mkStateTDead (\_ -> m)), s')
-
-  interleave ma mb = mkStateTDead $ \s ->
-                        runStateTDead ma s `interleave` runStateTDead mb s
-
-  ma >>- f = mkStateTDead $ \s ->
-                runStateTDead ma s >>- \(a,s') -> runStateTDead (f a) s'
-
-  ifte t th el = mkStateTDead $ \s ->
-                     ifte (runStateTDead t s)
-                          (\(a,s') -> runStateTDead (th a) s')
-                          (runStateTDead el s)
--}
--- Instead of using MonadLogic, which doesn't support
--- GeneralizedNewtypeDeriving, I'm just going to stick to MonadPlus
--- and roll my own non-determinism monad.
-
-newtype ND m a =
-  ND { unND :: forall r. (a -> m r -> m r) -> m r -> m r }
-
-runND :: ND Identity a -> [a]
-runND (ND f) = runIdentity $
-  f (\a (Identity as) -> Identity (a:as)) (Identity [])
-
-instance Monad (ND m) where
-  return a = ND (\k -> k a)
-  ND f >>= g = ND (\k -> f (\a -> unND (g a) k))
-
-instance Applicative (ND m) where
-  pure = return
-  (<*>) = ap
-
-instance Functor (ND m) where
-  fmap f (ND g) = ND (\k -> g (\a -> k (f a)))
-
-instance MonadPlus (ND m) where
-  mzero = ND (\sk fk -> fk)
-  ND f `mplus` ND g = ND (\sk fk -> f (\a fk' -> sk a fk')
-                                      (g (\a fk' -> sk a fk') fk))
-
-instance Alternative (ND m) where
-  empty = mzero
-  (<|>) = mplus
